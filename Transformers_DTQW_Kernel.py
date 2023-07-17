b4cb972d8d765d49c9dae5ec4bc462bf615640ee
Amit <66776279+amit1221levi@users.noreply.github.com>
2023-07-17 22:49:45 +0200

    Update Transformers_DTQW_Kernel.py

--- Transformers_DTQW_Kernel.py
+++ Transformers_DTQW_Kernel.py
@@ -3,52 +3,52 @@ from torch import nn
 from torch.nn import functional as F
 from longformer.sliding_chunks import sliding_chunks_matmul_qk, sliding_chunks_matmul_pv
 
-# Sparse Attention class
 class SparseAttention(nn.Module):
+    """
+    Sparse Attention class that implements the sliding window-based sparse attention.
+    """
     def __init__(self, attention_window, hidden_size, num_heads):
         super().__init__()
-        # Attention window size, hidden size, and number of attention heads are defined
         self.attention_window = attention_window
         self.hidden_size = hidden_size
         self.num_heads = num_heads
 
     def forward(self, query, key, value, mask=None):
         batch_size, seq_len, _ = query.size()
-        # Check if the sequence length is a multiple of the attention window size
-        assert seq_len % (self.attention_window * 2) == 0, "Sequence length must be a multiple of attention window size"
-        
-        # Reshape the input tensors for multi-head attention computation
-        query = query.view(batch_size, seq_len, self.num_heads, self.hidden_size // self.num_heads).transpose(1, 2)
-        key = key.view(batch_size, seq_len, self.num_heads, self.hidden_size // self.num_heads).transpose(1, 2)
-        value = value.view(batch_size, seq_len, self.num_heads, self.hidden_size // self.num_heads).transpose(1, 2)
 
-        # Apply the mask if provided
+        if seq_len % (self.attention_window * 2) != 0:
+            raise ValueError("Sequence length must be a multiple of attention window size")
+
+        def reshape(input_tensor):
+            return input_tensor.view(batch_size, seq_len, self.num_heads, self.hidden_size // self.num_heads).transpose(1, 2)
+
+        query, key, value = map(reshape, (query, key, value))
+
         if mask is not None:
-            mask = mask.unsqueeze(1).repeat(1, self.num_heads, 1, 1)
-        
-        # Compute attention weights using sliding_chunks_matmul_qk function
-        # This function reduces computational complexity from O(N^2) to O(N * attention_window)
+            mask = mask.unsqueeze(1).expand(-1, self.num_heads, -1, -1)
+
         attn_output_weights = sliding_chunks_matmul_qk(query, key, self.attention_window, padding_value=0, mask=mask)
         attn_output_weights = F.softmax(attn_output_weights, dim=-1)
 
-        # Compute the final attention output using sliding_chunks_matmul_pv function
         attn_output = sliding_chunks_matmul_pv(attn_output_weights, value, self.attention_window)
         attn_output = attn_output.transpose(1, 2).contiguous().view(batch_size, seq_len, self.hidden_size)
-        
+
         return attn_output
 
-# Transformer Layer with Sparse Attention
+
 class TransformerLayer(nn.Module):
+    """
+    Transformer Layer class that includes a Sparse Attention module.
+    """
     def __init__(self, hidden_size, num_heads, attention_window, feedforward_dim, dropout):
         super().__init__()
-        # Instantiate SparseAttention
+
         self.self_attn = SparseAttention(attention_window, hidden_size, num_heads)
-        # Add Layer Normalization and Dropout
+
         self.norm1 = nn.LayerNorm(hidden_size)
         self.norm2 = nn.LayerNorm(hidden_size)
         self.dropout = nn.Dropout(dropout)
 
-        # Add the feed-forward layer
         self.feed_forward = nn.Sequential(
             nn.Linear(hidden_size, feedforward_dim),
             nn.ReLU(),
@@ -57,30 +57,152 @@ class TransformerLayer(nn.Module):
         )
 
     def forward(self, src, mask=None):
-        # Apply SparseAttention to the input
         attn_output = self.self_attn(src, src, src, mask)
-        src = src + self.dropout(attn_output)
-        src = self.norm1(src)
+        src = self.norm1(src + self.dropout(attn_output))
 
-        # Apply the feed-forward layer
         ff_output = self.feed_forward(src)
-        src = src + self.dropout(ff_output)
-        src = self.norm2(src)
+        src = self.norm2(src + self.dropout(ff_output))
 
         return src
 
-# Integrate the SparseAttention into the Transformer model
-# Replace standard attention layers with TransformerLayer containing SparseAttention
+class TransformerModel(nn.Module):
+    """
+    Transformer model that incorporates a TransformerLayer with Sparse Attention.
+    """
+    def __init__(self, ntoken, hidden_size, nhead, nlayers, attention_window, feedforward_dim, dropout=0.5):
+        super(TransformerModel, self).__init__()
+
+        self.model_type = 'Transformer'
+        self.hidden_size = hidden_size
+        self.dropout = nn.Dropout(dropout)
+        self.embedding = nn.Embedding(ntoken, hidden_size)
+
+        self.transformer_layers = nn.ModuleList([
+            TransformerLayer(hidden_size, nhead, attention_window, feedforward_dim, dropout) 
+            for _ in range(nlayers)
+        ])
+
+        self.decoder = nn.Linear(hidden_size, ntoken)
+
+        self.init_weights()
+
+    def init_weights(self):
+        initrange = 0.1
+        self.embedding.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, input, mask=None):
+        embedded = self.dropout(self.embedding(input))
+
+        output = embedded
+        for transformer_layer in self.transformer_layers:
+            output = transformer_layer(output, mask)
+
+        output = self.decoder(output)
+
+        return F.log_softmax(output, dim=-1)
+import torch
+from torch import nn
+from torch.nn import functional as F
+from longformer.sliding_chunks import sliding_chunks_matmul_qk, sliding_chunks_matmul_pv
+
+class SparseAttention(nn.Module):
+    """
+    Sparse Attention class that implements the sliding window-based sparse attention.
+    """
+    def __init__(self, attention_window, hidden_size, num_heads):
+        super().__init__()
+        self.attention_window = attention_window
+        self.hidden_size = hidden_size
+        self.num_heads = num_heads
+
+    def forward(self, query, key, value, mask=None):
+        batch_size, seq_len, _ = query.size()
+
+        if seq_len % (self.attention_window * 2) != 0:
+            raise ValueError("Sequence length must be a multiple of attention window size")
+
+        def reshape(input_tensor):
+            return input_tensor.view(batch_size, seq_len, self.num_heads, self.hidden_size // self.num_heads).transpose(1, 2)
+
+        query, key, value = map(reshape, (query, key, value))
+
+        if mask is not None:
+            mask = mask.unsqueeze(1).expand(-1, self.num_heads, -1, -1)
+
+        attn_output_weights = sliding_chunks_matmul_qk(query, key, self.attention_window, padding_value=0, mask=mask)
+        attn_output_weights = F.softmax(attn_output_weights, dim=-1)
+
+        attn_output = sliding_chunks_matmul_pv(attn_output_weights, value, self.attention_window)
+        attn_output = attn_output.transpose(1, 2).contiguous().view(batch_size, seq_len, self.hidden_size)
+
+        return attn_output
 
 
-'''
-This implementation aims to improve the standard Transformer model by incorporating sparse attention and quantum walk-based attention mechanisms. The primary goal is to enhance the model's performance while reducing computational complexity, leading to faster training and the ability to handle larger sequence lengths.
+class TransformerLayer(nn.Module):
+    """
+    Transformer Layer class that includes a Sparse Attention module.
+    """
+    def __init__(self, hidden_size, num_heads, attention_window, feedforward_dim, dropout):
+        super().__init__()
 
-Sparse Attention:
-The standard Transformer model uses self-attention mechanisms that have a quadratic computational complexity (O(N^2)) with respect to the input sequence length (N). This becomes computationally expensive, especially when dealing with long sequences. Sparse attention addresses this issue by leveraging a sliding window approach to limit the attention span to a local context. By doing so, the computational complexity is reduced from O(N^2) to O(N * attention_window), where attention_window is the size of the local context window. This reduction in complexity allows the model to scale better to longer sequences and train faster.
+        self.self_attn = SparseAttention(attention_window, hidden_size, num_heads)
 
-Quantum Walk-Based Attention:
-Quantum walk-based attention mechanisms are inspired by the properties of quantum systems and discrete-time quantum walks (DTQW). The goal is to enhance the model's ability to capture long-range dependencies and improve its overall performance. A DTQW is defined on the graph representing the input sequence, and the Grover diffusion operator is used as the basis for updating the attention weights in the Transformer model. By integrating the quantum walk-based attention mechanism into the Transformer architecture, it is expected to leverage the strengths of both quantum and classical approaches, potentially leading to better performance on various natural language processing tasks.
+        self.norm1 = nn.LayerNorm(hidden_size)
+        self.norm2 = nn.LayerNorm(hidden_size)
+        self.dropout = nn.Dropout(dropout)
 
-In summary, this proposed model aims to enhance the standard Transformer by combining the benefits of both sparse attention and quantum walk-based attention mechanisms. The sparse attention mechanism addresses the computational complexity issues, while the quantum walk-based attention mechanism seeks to improve the model's ability to capture long-range dependencies. This combined approach could potentially lead to a more efficient and high-performing model in natural language processing tasks compared to the classic Transformer architecture.
-'''
+        self.feed_forward = nn.Sequential(
+            nn.Linear(hidden_size, feedforward_dim),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(feedforward_dim, hidden_size),
+        )
+
+    def forward(self, src, mask=None):
+        attn_output = self.self_attn(src, src, src, mask)
+        src = self.norm1(src + self.dropout(attn_output))
+
+        ff_output = self.feed_forward(src)
+        src = self.norm2(src + self.dropout(ff_output))
+
+        return src
+
+class TransformerModel(nn.Module):
+    """
+    Transformer model that incorporates a TransformerLayer with Sparse Attention.
+    """
+    def __init__(self, ntoken, hidden_size, nhead, nlayers, attention_window, feedforward_dim, dropout=0.5):
+        super(TransformerModel, self).__init__()
+
+        self.model_type = 'Transformer'
+        self.hidden_size = hidden_size
+        self.dropout = nn.Dropout(dropout)
+        self.embedding = nn.Embedding(ntoken, hidden_size)
+
+        self.transformer_layers = nn.ModuleList([
+            TransformerLayer(hidden_size, nhead, attention_window, feedforward_dim, dropout) 
+            for _ in range(nlayers)
+        ])
+
+        self.decoder = nn.Linear(hidden_size, ntoken)
+
+        self.init_weights()
+
+    def init_weights(self):
+        initrange = 0.1
+        self.embedding.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, input, mask=None):
+        embedded = self.dropout(self.embedding(input))
+
+        output = embedded
+        for transformer_layer in self.transformer_layers:
+            output = transformer_layer(output, mask)
+
+        output = self.decoder(output)
+
+        return F.log_softmax(output, dim=-1)
